4a9f41d3a9f949d4bdf3ed74a07e4293d4732519
rathnathejas17 <rathnathejas@gmail.com>
2025-09-02 18:46:42 +0530

    this is my second commit

--- javacode.java
+++ javacode.java
@@ -3,3 +3,18 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create Scanner object to read input
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();  // Read a line of text
+
+        System.out.println("Hello, " + name + "! Welcome to Java.");
+        
+        scanner.close();  // Always close the scanner
+    }
+}
